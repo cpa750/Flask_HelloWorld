8bf2c718b8fd4cc56108bd201ebaf0ee284d3050
Cian <ciantheskier@gmail.com>
2018-10-09 18:50:56 +0100

    Added and intialized the db

---
+++ __init__.py
@@ -0,0 +1,36 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    """
+    Creating and configuring the app.
+    """
+
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = "dev",
+        DATABASE = os.path.join(app.instance_path, "flask_HelloWorld.sqlite"),
+    )
+
+    if test_config is None:
+        # If there is no testing configuration, configure from config.py
+        app.config.from_pyfile("config.py", silent=True)
+
+    else:
+        # Else load the test configuration if passed in
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/hello")
+    def hello():
+        return "Hello world!"
+
+    from . import db
+    db.init_app(app)
+
+    return app
